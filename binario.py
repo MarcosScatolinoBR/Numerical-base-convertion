a096d6636f0171eef9ab5f0abbaca0e0d2fe2526
MarcosScatolinoBR <88802032+MarcosScatolinoBR@users.noreply.github.com>
2022-04-04 11:21:48 -0300

    mudei um pouco

--- binario.py
+++ binario.py
@@ -1,17 +1,15 @@
 binario = []
+entrou = ''
 def entra():
+    global entrou
     entrou = input('Entre com um número de, no máximo, 3 dígitos, em forma binária: ')
-    if (len(entrou) > 3):
-        print('Entre com um número de no máximo de 3 dígitos')
-        entra()
+    testa = len(entrou)
+    if (testa > 3):
+        print('Entre com um número de no máximo 3 dígitos')
+        return entra()
     for i in entrou:
-        if (entrou != 0 or entrou != 1):
-        print(i)
-        print('Digite apenas números 0 ou 1')
-        entra()
-    else:
-        entrada = int(entrou)
-        binario = bin(entrada)
-
+        if (i == 0):
+            print(i)
+        elif (i == 1):
+            print(i)
 entra()
-print(binario)
